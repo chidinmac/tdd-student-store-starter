f79b65247c9faae25cd9a4c9e9ae98ab0f3b2279
chidinmac <87202479+chidinmac@users.noreply.github.com>
2022-07-01 18:03:04 -0700

    Add files via upload

--- student-store-ui/src/components/Sidebar/Sidebar.jsx
+++ student-store-ui/src/components/Sidebar/Sidebar.jsx
@@ -1,10 +1,45 @@
-import * as React from "react"
-import "./Sidebar.css"
+import * as React from "react";
+import { useState } from "react";
+import CheckOutForm from "../CheckOutForm/CheckOutForm";
+import "./Sidebar.css";
 
-export default function Sidebar() {
+export default function Sidebar({
+  btn_status,
+  setbtn_status,
+  onAdd,
+  cartItems,
+  onRemove,
+  setCartItems
+}) {
+
+  let cnt = 0;
+  const [isActive, setActive] = useState("false");
+  //When button is clicked set button status to visible, which makes the sidebar open, also set isActive to true
+  //If the  button is already open, close it by reseting setting isActive to false
+  function handleOnClick() {
+    setbtn_status("visible");
+    setActive("true");
+    cnt++;
+    if (cnt > 1) {
+      cnt = 0;
+      setActive("false");
+    }
+  }
   return (
-    <section className="sidebar">
-      <p>Sidebar</p>
+    <section className={isActive == "true" ? btn_status : null} id={"sidebar"}>
+      <div className="sidebar-btn" id={btn_status} onClick={handleOnClick}>
+        <span></span>
+        <span></span>
+        <span></span>
+      </div>
+      {isActive == "true" ? (
+        <CheckOutForm
+        onAdd={onAdd}
+        cartItems={cartItems}
+        onRemove={onRemove}
+        setCartItems={setCartItems}
+        />
+      ) : null}
     </section>
-  )
+  );
 }
