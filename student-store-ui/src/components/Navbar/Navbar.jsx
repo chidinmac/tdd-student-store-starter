0bb620e7eb1c5f9af3e67ce2f183ec545a7c65ba
chidinmac <87202479+chidinmac@users.noreply.github.com>
2022-07-01 18:02:09 -0700

    Add files via upload

--- student-store-ui/src/components/Navbar/Navbar.jsx
+++ student-store-ui/src/components/Navbar/Navbar.jsx
@@ -1,10 +1,25 @@
-import * as React from "react"
-import "./Navbar.css"
+import * as React from "react";
+import Products from "../Products/Products";
+import { Routes, Route } from "react-router";
+import "./Navbar.css";
 
-export default function Navbar() {
+export default function Navbar({ navLinks }) {
   return (
     <nav className="navbar">
-      <p>Navbar</p>
+      <NavLinks navLinks={navLinks} />
     </nav>
-  )
+  );
+}
+export function NavLinks({ navLinks }) {
+  return (
+
+    <ul className="nav-links">
+      {navLinks.map((link) => (
+        <NavLink key={link.label} navLink={link} />
+      ))}
+    </ul>
+  );
+}
+export function NavLink({ navLink }) {
+  return <li className="navLink">{navLink.label}</li>;
 }
